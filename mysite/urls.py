136d27559716fde65aa461d56c13f821a89175a2
scelanetip <scela@netipbox.com>
2019-08-11 16:15:32 +0200

    first view

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,7 @@
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import include, path
 
 urlpatterns = [
-    path('autotag/', include('autotag.urls')),
     path('admin/', admin.site.urls),
+    path('', include('autotag.urls')),
 ]
