94419d65550736f93b02444ff6eeba0a8cd61de2
scelanetip <scela@netipbox.com>
2019-09-25 11:27:01 +0200

    adapted to vinals

--- autotag/views.py
+++ autotag/views.py
@@ -3,10 +3,12 @@ import os
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from static.autotag import autotag
+from static.autotag import autotag, autotag_video, autotag_txt, detect_document
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+IMG_EXTENSION = ['.bmp', '.jpe', '.jpg', '.jpeg', '.tif', '.gif', '.ppm', '.rgb', '.pgm', '.png', '.pnm']
+VID_EXTENSION = ['.mpeg', '.mov', '.mpg', '.mpe', '.avi', '.movie', '.mp4']
 
 
 def index(request):
@@ -16,7 +18,14 @@ def index(request):
 @csrf_exempt
 def predict(request):
 
-    img_data = request.FILES['file'].read()
-    labels = autotag(img_data)
+    data = request.FILES['file'].read()
+    filename, file_extension = os.path.splitext(request.FILES['file'].name)
+    labels = ['vinals']
+    if file_extension in IMG_EXTENSION:
+        labels = labels + ['imagen'] + detect_document(data) + autotag(data)
+    elif file_extension in VID_EXTENSION:
+        labels = labels + ['video'] + autotag_video(data) + autotag_txt(data)
+    else:
+        labels = ['not valid file extension']
 
     return JsonResponse({'labels': labels})
--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -125,6 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'www', 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+print(os.path.join(BASE_DIR, 'static'))
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
